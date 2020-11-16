2374180b0e91af194dc6e2d41e9075d3a6c0ef4d
vulkan-ops <vulkanops@gmail.com>
2020-11-16 20:35:27 -0300

    Import initial configs

---
+++ configure.sh
@@ -0,0 +1,65 @@
+files/#!/bin/bash
+
+ln -s /hostlvm /run/lvm
+
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+sed -i "s/#Color/Color/g" /etc/pacman.conf
+
+modprobe zram
+echo lz4 > /sys/block/zram0/comp_algorithm
+sed -i "s/0/32GB/g" /sys/block/zram0/disksize
+mkswap --label zram0 /dev/zram0
+swapon --priority 100 /dev/zram0
+
+echo "Config mirrors"
+rm -rf /etc/pacman.d/mirrorlist
+mv files/mirrorlist /etc/pacman.d/
+
+sudo pacman -Sy efibootmgr git grub nano refind sudo wget --needed --noconfirm
+
+echo "Set locale and zone"
+sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
+sed -i "s/#en_US ISO-8859-1/en_US ISO-8859-1/g" /etc/locale.gen
+sed -i "s/#pt_BR.UTF-8 UTF-8/pt_BR.UTF-8 UTF-8/g" /etc/locale.gen
+sed -i "s/#pt_BR ISO-8859-1/pt_BR ISO-8859-1/g" /etc/locale.gen
+locale-gen
+echo LANG=pt_BR.UTF-8 > /etc/locale.conf
+echo aspire > /etc/hostname
+sudo ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+
+echo "Adding my user"
+useradd -m -G wheel -s /bin/bash vitor && chmod +x /home/vitor
+clear
+passwd vitor
+passwd root
+chown -R vitor:vitor /home/vitor
+chmod +x /home/vitor
+
+git clone https://github.com/vulkan-ops/dotfiles /home/vitor/.dotfiles
+
+echo "Config sudoers"
+rm -rf /etc/sudoers
+mv files/sudoers /etc/
+
+echo "Config mkinitcpio"
+rm -rf /etc/mkinitcpio.conf
+mv files/mkinitcpio.conf /etc/
+mkinitcpio -p linux
+
+lsblk -fo +partuuid
+
+echo "Config grub"
+UUID=$(blkid /dev/sda2 | awk -F '"' '{print $2}')
+sed -i "s/SEU_ID_AQUI/$UUID/g" files/grub
+rm -rf /etc/default/grub
+mv files/grub /etc/default
+
+pacman -Sy iwc --noconfirm
+systemctl enable iwd
+
+echo "Config grub"
+refind-install --usedefault /dev/sda1
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "exit / umount -R / reboot"
---
+++ format.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkfs.fat -F32 /dev/sda1
+
+cryptsetup luksFormat -c aes-xts-plain64 -s 512 -h sha512 --use-random -i 100 /dev/sda2
+
+cryptsetup luksOpen /dev/sda2 lvm
+
+pvcreate /dev/mapper/lvm
+vgcreate arch /dev/mapper/lvm
+
+lvcreate -L 100G arch -n root
+lvcreate -L 8G arch -n swap
+lvcreate -l 100%FREE arch -n home
+
+mkfs.ext4 /dev/mapper/arch-root
+mkfs.ext4 /dev/mapper/arch-home
+mkswap /dev/mapper/arch-swap
+
+mount /dev/mapper/arch-root /mnt
+
+mkdir /mnt/home
+mkdir /mnt/boot
+mkdir -p /mnt/media/storage
+mkdir /mnt/hostlvm
+
+mount /dev/mapper/arch-home /mnt/home
+mount /dev/sda1 /mnt/boot
+
+swapon /dev/mapper/arch-swap
+mount --bind /run/lvm /mnt/hostlvm
